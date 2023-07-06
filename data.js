0bed3654dfbc2742bb1d73b88e802ccf68fb0ae5
Richard <richard_stephens@hotmail.co.uk>
2023-07-06 06:34:13 +0100

    initial commit

---
+++ data.js
@@ -0,0 +1,39 @@
+let grid = [
+  {
+    colour: "all",
+    items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  },
+  {
+    colour: "blue",
+    items: [1, 5, 6, 7, 10],
+  },
+  {
+    colour: "purple",
+    items: [2, 4, 7, 8, 9],
+  },
+  {
+    colour: "mint",
+    items: [3, 5, 6, 9, 10],
+  },
+  {
+    colour: "odd",
+    items: [1, 3, 5, 7, 9],
+  },
+  {
+    colour: "even",
+    items: [2, 4, 6, 8, 10],
+  },
+];
+
+let numbersList = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+];
